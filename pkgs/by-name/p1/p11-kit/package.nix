64a69dc538e08b883dbfc345accaf302a3a889ba
Piotr Grabowski <avelanarius@gmail.com>
2025-07-16 23:42:17 +0200

    Update package.nix

--- pkgs/by-name/p1/p11-kit/package.nix
+++ pkgs/by-name/p1/p11-kit/package.nix
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
     owner = "avelanarius";
     repo = "p11-kit";
     rev = version;
-    hash = "sha256-6zuvYnQAe27ynX5L9A6yWkADI9LZ1bWR6DHAey8+yQ=";
+    hash = "sha256-6zuvYnQAe27ynX5L9A6yWkAOI9LZ1bWR6OHAey8+yQ=";
     fetchSubmodules = true;
   };
 
